9ead9b21f16553b0515056e9ba2535baa550c463
Aeolus96 <devson.butani@gmail.com>
2025-10-16 18:14:40 -0400

    added comments

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup
 from glob import glob
+
+from setuptools import setup
 
 package_name = "edubot"
 
@@ -7,21 +8,30 @@ setup(
     name=package_name,
     version="1.0.0",
     packages=[package_name],
-    data_files=[  # Add all the files to be built and installed by colcon build
+    # Files we want to install with the package
+    data_files=[
+        # Install marker for ament to find this package - REQUIRED
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        # Install package.xml for ROS to find and setup dependencies - REQUIRED
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name, glob("launch/*.py")),
+        # Install launch files - OPTIONAL
+        ("share/" + package_name, glob("launch/*.launch.py")),
+        # Install all other files such as URDF, meshes, and config files - OPTIONAL
         ("share/" + package_name + "/urdf/", glob("urdf/*")),
         ("share/" + package_name + "/meshes/", glob("meshes/*")),
         ("share/" + package_name + "/config/", glob("config/*")),
     ],
+    # Python dependencies for this package, setuptools REQUIRED
     install_requires=["setuptools"],
     zip_safe=True,
+    author="Devson Butani",
+    author_email="dbutani@ltu.edu",
     maintainer="Devson Butani",
     maintainer_email="dbutani@ltu.edu",
     description="EduBot robot description package",
     license="MIT",
-    entry_points={  # Add executable scripts and their entry points here
+    # Add executable Python scripts and their entry points (usually 'main') here
+    entry_points={  # example: 'my_script = my_package.my_script:main'
         "console_scripts": [],
     },
 )
